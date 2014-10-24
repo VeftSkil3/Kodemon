bf047a6e3eac6de098bb1c0b6abf0904a6fadb9e
VeftSkil3 <margret.hr@gamil.com>
2014-10-24 20:36:41 +0000

    Create manage.py
    To create database and start app

---
+++ Measure/manage.py
@@ -0,0 +1,25 @@
+# manage.py
+
+from flask.ext.script import Manager
+
+from app import app
+from db import engine, Base
+from models import Movie
+
+manager = Manager(app)
+
+
+@manager.command
+def create_db():
+    Base.metadata.create_all(engine)
+
+@manager.command
+def sync_db():
+    Base.metadata.create_all(engine)
+
+@manager.command
+def debug():
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    manager.run()
