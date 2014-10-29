7061bf0f34ba3c6bd3628fe4329bdc452ee12c9a
VeftSkil3 <margret.hr@gamil.com>
2014-10-29 23:41:42 +0000

    Update global.js

--- Interface/public/javascripts/global.js
+++ Interface/public/javascripts/global.js
@@ -1,82 +1,57 @@
 // Functionlist data array for filling in selection box
 var functionListData = [];
 
-
 // DOM Ready =============================================================
 $(document).ready(function() {
-    // Populate the functionList table on initial page load
-    //populateTable();
-    
+    // Populate the functionList combo on initial page load
     populateCombo();
-    
-    // function link click
-    $('#functionList table tbody').on('click', 'td a.linkshowtimes', showFunctionTimes);
-
-
 });
-
 
 // Functions =============================================================
 
-
 // Fill table with data
-function populateTable() {
-    // Empty content string
-    var tableContent = '';
-    // jQuery AJAX call for JSON
-    $.getJSON( '/functions/functionlist', function( data ) {
-		// Stick our function data array into a functionlist variable in the global object
-		functionListData = data;
-
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowtimes" rel="' + this.Id + '" title="Show Details">' + /[^-]*$/.exec(this.Key)[0] + '</a></td>';
-            tableContent += '</tr>';
-        });;
-        // Inject the whole content string into our existing HTML table
-        $('#functionList table tbody').html(tableContent);
-    });
-};
-
-// Fill combo with data
 function populateCombo() {
-    // Empty content string
-    var comboContent = '';
-    var select = document.getElementById("thekeys");
+    // Get the combo
+    var combo = document.getElementById("combo");
     // jQuery AJAX call for JSON
     $.getJSON( '/functions/functionlist', function( data ) {
-		// Stick our function data array into a functionlist variable in the global object
-		functionListData = data;
+        // Stick our function data array into a functionlist variable in the global object
+        functionListData = data;
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
-			var option = document.createElement('option');
-			option.text = /[^/]*$/.exec(this.Key)[0];
-			option.value=this.Id;
-			select.add(option, 0);
+            var option = document.createElement("option");
+            option.text = /[^-]*$/.exec(this.Key)[0];
+            option.value = this.Id;
+            try {
+                combo.add(option, null); //Standard 
+            }catch(error) {
+                combo.add(option); // IE only
+            }
         });;
     });
 };
-
 
 function showFunctionTimes(key) {
-    //event.preventDefault();
 	// Empty content string
     var tableContent = '';
     var date1=document.getElementById("from").value;
     var date2=document.getElementById("to").value;
+
     if(document.getElementById("dates").checked){
         // jQuery AJAX call for JSON
-        $.getJSON( '/functions/times/' + key + '/' + date1 + '/' + date2, function( data ) {
-            // Stick our function data array into a functionlist variable in the global object
-            //functionTimesData = data;
+        if (date1 == "" || date2 == ""){
+            alert("Það verður að velja dagsetningar");
+            $('#functionTimes table tbody').html(tableContent);
+            return;
+        };
 
+        $.getJSON( '/functions/times/' + key + '/' + date1 + '/' + date2, function( data ) {
             // For each item in our JSON, add a table row and cells to the content string
             $.each(data, function(){
                 tableContent += '<tr>';
-                tableContent += '<td>' + this.DateTime + '</td>';
-                tableContent += '<td>' + this.ExecTime + '</td>';
+                tableContent += '<td>' + this.DateTime.slice(0, 10) +  "  " + this.DateTime.slice(12, 19) + '</td>';
+                tableContent += '<td>' + this.ExecTime.toFixed(4) + '</td>';
                 tableContent += '</tr>';
             });;
             // Inject the whole content string into our existing HTML table
@@ -86,14 +61,11 @@ function showFunctionTimes(key) {
     else {
         // jQuery AJAX call for JSON
         $.getJSON( '/functions/times/' + key, function( data ) {
-            // Stick our function data array into a functionlist variable in the global object
-            //functionTimesData = data;
-
             // For each item in our JSON, add a table row and cells to the content string
             $.each(data, function(){
                 tableContent += '<tr>';
-                tableContent += '<td>' + this.DateTime + '</td>';
-                tableContent += '<td>' + this.ExecTime + '</td>';
+                tableContent += '<td>' + this.DateTime.slice(0, 10) + "  " + this.DateTime.slice(12, 19) + '</td>';
+                tableContent += '<td>' + this.ExecTime.toFixed(4) + '</td>';
                 tableContent += '</tr>';
             });;
             // Inject the whole content string into our existing HTML table
