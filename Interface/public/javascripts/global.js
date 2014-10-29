89e64c2d12097804163118664e0953d523c397e0
VeftSkil3 <margret.hr@gamil.com>
2014-10-29 02:03:18 +0000

    Skoða færslur eftir tímabilum

--- Interface/public/javascripts/global.js
+++ Interface/public/javascripts/global.js
@@ -10,15 +10,7 @@ $(document).ready(function() {
     // function link click
     $('#functionList table tbody').on('click', 'td a.linkshowtimes', showFunctionTimes);
 
-	//var nowTime = new Date();
-    //nowTime.setHours(8);
-    //nowTime.setMinutes(50);
-    //alert(nowTime.toLocaleString());
-    //$("#Date1").datebox('setTheDate', nowTime).trigger('datebox', {'method':'doset'});
-    //$("#Date2").datebox('setTheDate', nowTime).trigger('datebox', {'method':'doset'});
-    //var dateObject = $('#Date1').datebox('getTheDate'),
-    //theDate =  $('#Date1').datebox('callFormat', '%d/%m/%Y', dateObject);
-	//alert(theDate);
+
 });
 
 
@@ -37,7 +29,7 @@ function populateTable() {
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowtimes" rel="' + this.Id + ' "title="Show Details">' + /[^-]*$/.exec(this.Key)[0] + '</a></td>';
+            tableContent += '<td><a href="#" class="linkshowtimes" rel="' + this.Id + '" title="Show Details">' + /[^-]*$/.exec(this.Key)[0] + '</a></td>';
             tableContent += '</tr>';
         });;
         // Inject the whole content string into our existing HTML table
@@ -49,20 +41,41 @@ function populateTable() {
 function showFunctionTimes(event) {
     event.preventDefault();
 	// Empty content string
-    var tableContent2 = '';
-    // jQuery AJAX call for JSON
-    $.getJSON( '/functions/times/' + $(this).attr('rel'), function( data ) {
-        // Stick our function data array into a functionlist variable in the global object
-		//functionTimesData = data;
+    var tableContent = '';
+    var date1=document.getElementById("from").value;
+    var date2=document.getElementById("to").value;
+    if(document.getElementById("dates").checked){
+        // jQuery AJAX call for JSON
+        $.getJSON( '/functions/times/' + $(this).attr('rel') + '/' + date1 + '/' + date2, function( data ) {
+            // Stick our function data array into a functionlist variable in the global object
+            //functionTimesData = data;
 
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent2 += '<tr>';
-            tableContent2 += '<td>' + this.DateTime + '</td>';
-            tableContent2 += '<td>' + this.ExecTime + '</td>';
-            tableContent2 += '</tr>';
-        });;
-        // Inject the whole content string into our existing HTML table
-        $('#functionTimes table tbody').html(tableContent2);
-    });
+            // For each item in our JSON, add a table row and cells to the content string
+            $.each(data, function(){
+                tableContent += '<tr>';
+                tableContent += '<td>' + this.DateTime + '</td>';
+                tableContent += '<td>' + this.ExecTime + '</td>';
+                tableContent += '</tr>';
+            });;
+            // Inject the whole content string into our existing HTML table
+            $('#functionTimes table tbody').html(tableContent);
+        });
+    }
+    else {
+        // jQuery AJAX call for JSON
+        $.getJSON( '/functions/times/' + $(this).attr('rel'), function( data ) {
+            // Stick our function data array into a functionlist variable in the global object
+            //functionTimesData = data;
+
+            // For each item in our JSON, add a table row and cells to the content string
+            $.each(data, function(){
+                tableContent += '<tr>';
+                tableContent += '<td>' + this.DateTime + '</td>';
+                tableContent += '<td>' + this.ExecTime + '</td>';
+                tableContent += '</tr>';
+            });;
+            // Inject the whole content string into our existing HTML table
+            $('#functionTimes table tbody').html(tableContent);
+        });
+    }
 };
