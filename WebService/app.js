18a65c21fe70b7a88c9a259fdba33177a4ae5388
VeftSkil3 <odinn_b@hotmail.com>
2014-11-04 21:20:39 +0000

    Comment

--- WebService/app.js
+++ WebService/app.js
@@ -3,31 +3,29 @@ mongoose = require('mongoose');
 
 var app = express();
 
-// Database
+// Tengi inn gagnagrunn
 mongoose.connect('mongodb://localhost/test');
-
+//Skilgreini gögnin
 var KodemonSchema=mongoose.Schema({
 	execution_time: Number,
 	timestamp: Date,
 	token: String,
 	key: String
 });
-
 var MonData=mongoose.model('MonData',KodemonSchema);
 var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function callback () {
 	console.log('Connected to MongoDB');
 });
 
+//Grunnslóð vefþjónustunnar Hér væri hægt að koma með uppl um hvaða þjónusta standi til boða 
 app.get('/api', function (req, res) {
 	res.send('API is running');
 });
 
 
-// Returns json object containing distinct keys and the top _id for each key 
+// Náum í einkvæmar keys færslur ásamt fyrsta _ID á viðkoamndi keys og skilum í json object
 app.get('/api/kodemon/keys', function (req, res){
 	return MonData.distinct('key', function (err, mondatas){
 		if (err){
@@ -58,7 +56,7 @@ app.get('/api/kodemon/keys', function (req, res){
 });
 
 
-//Returns json object containing execution time and date for a key
+//Skilum öllum keyrslutímum og dagsetningum fyrir valinn key og skilum í json object
 app.get('/api/kodemon/key/times/:id', function (req, res){
 		MonData.findById(req.params.id,function (err, mondatas){
 			if (err){
@@ -80,7 +78,7 @@ app.get('/api/kodemon/key/times/:id', function (req, res){
 	});
 });
 
-//Returns json object containing execution time and date for a key filtered for a given date
+//Skilum öllum keyrslutímum og dagsetningum fyrir valinn key miðað við valda tímasetningu frá og til og skilum í json object
 app.get('/api/kodemon/key/times/:id/:datefrom/:dateto', function (req, res){
 		var start = new Date(req.params.datefrom);
 		var end = new Date(req.params.dateto);
@@ -105,26 +103,23 @@ app.get('/api/kodemon/key/times/:id/:datefrom/:dateto', function (req, res){
 	});
 });
 
-//Sends the result back to the website 
+//Sendum niðurstöðu fyrirspurnar til baka
 function sendResult(res, obj) {
   console.log(obj);
-
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(obj));
   return res.send();
 }
 
-//Sends error message back to the server 
+//Sendum villuboð til baka 
 function sendErrorResult(res, err){
   console.log(err);
-
   res.writeHead(400, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(err));
   return res.send();
-
 }
 
-// Launch server
+// vefþjónusta keyrð upp á porti 4242
 app.listen(4242, function(){
 	console.log('WebServer is running');
 });
